737d46e1141b6f1958f7dd1ca45feedf6244b69e
Akash9250 <akkisingh9875@gmail.com>
2026-08-19 14:50:39 +0530

    day4

---
+++ day4/day4.js
@@ -0,0 +1,13 @@
+let str = "akashsingh";
+
+let upper = str.toUpperCase();
+console.log("Uppercase:", upper);
+
+
+let lower = str.toLowerCase();
+console.log("Lowercase:", lower);
+let jv = "I am learning Javascript"
+let idx = jv.indexOf("java")
+console.log("Index of 'java':", idx);
+newIdx = jv.lastIndexOf("java")
+console.log("Last index is")

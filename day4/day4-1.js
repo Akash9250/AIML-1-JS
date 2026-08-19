49fb59eeaf2708d65c12c1417efd75e832eb9c98
Akash9250 <akkisingh9875@gmail.com>
2026-08-19 14:50:28 +0530

    day4

---
+++ day4/day4-1.js
@@ -0,0 +1,53 @@
+// var a="String is Good";
+// console.log(
+//     a.includes("Good")
+// )
+// let array=[10,20,30,40,50];
+// array.push(60);
+// array.pop();
+// array.unshift(5);
+// array.shift();
+
+// console.log(array)
+let date = new Date();
+console.log(
+    "Date =",
+    date.getDate()
+);
+
+console.log(
+    "Month =",
+    date.getMonth()+1
+);
+
+
+console.log(
+    "Year =",
+    date.getFullYear()
+);
+
+console.log(
+    "Time =",
+    date.getTime()
+);
+
+
+
+
+console.log(
+    "Maximum =",
+    Math.max(10,20,30,40)
+);
+
+console.log(
+    "Minimum =",
+    Math.min(10,20,30,40)
+);
+console.log(
+    "round =",
+    Math.round(5.6)
+);
+console.log(
+    "SquareRoot =",
+    Math.sqrt(10)
+);

ce7dac3259cd95b1cdf76385fd69c84d7ce4b63b
Akash9250 <akkisingh9875@gmail.com>
2026-08-19 14:49:44 +0530

    day2

---
+++ day2/index.js
@@ -0,0 +1,30 @@
+
+
+// var a=20;
+// var b=20;
+// var c=20;
+// console.log(a+b+c);
+
+
+// var x=20;
+// if(x==20){
+//     console.log("x is 20");
+// }
+
+// let name=prompt("Enter your Name: ");
+// if(name==Akash){
+//     console.log("Executed")
+// }
+
+// for(var i=0;i<=5;i++){
+//     console.log(i)
+// }
+
+// var x=19;
+// const arr=[1,2,3,4,5];
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+// console.log(arr[2]);
+const app = require('./app.js')
+console.log(app.xz)
